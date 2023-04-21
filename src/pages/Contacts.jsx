72f9f74472e7c2c69c51aa1f7586abd5b860731c
serheihimself <sergm080@gmail.com>
2023-04-21 14:24:49 +0300

    pages commit

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,34 @@
+import ContactForm from '../components/ContactForm/ContactForm';
+import ContactList from '../components/ContactList/ContactList';
+import Filter from '../components/Filter/Filter';
+import Loader from '../components/Loader/Loader';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from '../redux/contacts/operations';
+import { selectIsLoading, selectError } from '../redux/contacts/selectors';
+
+export default Contacts = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <title>Contacts</title>
+      <div>
+        <h2 title="Phonebook">
+          <ContactForm />
+        </h2>
+        <h3 title="Contacts">
+          <Filter />
+          {isLoading && !error && <Loader />}
+          <ContactList />
+        </h3>
+      </div>
+    </>
+  );
+};
